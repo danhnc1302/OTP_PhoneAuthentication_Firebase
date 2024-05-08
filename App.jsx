69a7cf1ef6c65f4b71b7d1ae8581116b0d52c609
danhnc <danhnc1302@gmail.com>
2024-05-08 21:54:02 +0700

    Firebase settings

---
+++ App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+function App() {
+
+  return (
+    <View style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+      <Text>Danh</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+});
+
+export default App;
